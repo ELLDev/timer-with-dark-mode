b53c971bb5ec39d7366c3ec2f1f470bbad818000
ratoobi <69602231+ratoobi@users.noreply.github.com>
2022-04-27 22:21:27 -0300

    Improve timer accuracy, add functionality to increment/decrement time buttons and add base logic to sound selector buttons

--- js/script.js
+++ js/script.js
@@ -2,44 +2,98 @@ const minutes = document.querySelector(".minutes");
 const seconds = document.querySelector(".seconds");
 const playButton = document.querySelector(".play");
 const pauseButton = document.querySelector(".pause");
+const stopButton = document.querySelector(".stop");
+const incrementButton = document.querySelector(".increment");
+const decrementButton = document.querySelector(".decrement");
+let soundButtons = document.querySelectorAll(".sound-selector button");
 let isCountDownActive = false;
+let elapsedMilliseconds = 1000;
+let activeSound;
 
-playButton.addEventListener("click", () => {
-  isCountDownActive = true;
-  playButton.classList.add("hide");
-  pauseButton.classList.remove("hide");
+selectSound = (button) => {
+  if (button.classList.contains("down")) {
+    activeSound.classList.remove("down");
+    activeSound = null;
+    button.classList.remove("down");
+    button.classList.add("up");
+  } else {
+    if (activeSound) {
+      activeSound.classList.remove("down");
+      activeSound.classList.add("up");
+    }
+    activeSound = button;
+    button.classList.add("down");
+    button.classList.remove("up");
+  }
+};
+
+soundButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    selectSound(button);
+  });
 });
 
-pauseButton.addEventListener("click", () => {
+pauseTimer = () => {
   isCountDownActive = false;
   playButton.classList.remove("hide");
   pauseButton.classList.add("hide");
+};
+
+playButton.addEventListener("click", () => {
+  if (Number(minutes.textContent) > 0 || Number(seconds.textContent) > 0) {
+    isCountDownActive = true;
+    playButton.classList.add("hide");
+    pauseButton.classList.remove("hide");
+  }
 });
 
-function countdown() {
+pauseButton.addEventListener("click", () => {
+  pauseTimer();
+});
+
+stopButton.addEventListener("click", () => {
+  pauseTimer();
+  seconds.textContent = "00";
+  minutes.textContent = "00";
+});
+
+incrementButton.addEventListener("click", () => {
+  minutes.textContent = `${Number(minutes.textContent) + 5}`.padStart(2, 0);
+});
+
+decrementButton.addEventListener("click", () => {
+  if (Number(minutes.textContent) > 0) {
+    minutes.textContent = `${Number(minutes.textContent) - 5}`.padStart(2, 0);
+  }
+});
+
+countdown = () => {
   setTimeout(() => {
     if (isCountDownActive) {
-      switch (Number(seconds.textContent)) {
-        case 1:
-          seconds.textContent = "00";
-          if (Number(minutes.textContent) === 0) {
-            return;
-          }
-          break;
-
-        case 0:
-          if (Number(minutes.textContent) > 0) {
-            seconds.textContent = "59";
-            minutes.textContent = `${Number(minutes.textContent) - 1}`.padStart(2, 0);
-          }
-          break;
-
-        default:
-          seconds.textContent = `${Number(seconds.textContent) - 1}`.padStart(2, 0);
+      elapsedMilliseconds -= 250;
+      if (elapsedMilliseconds === 0) {
+        elapsedMilliseconds = 1000;
+        switch (Number(seconds.textContent)) {
+          case 1:
+            seconds.textContent = "00";
+            if (Number(minutes.textContent) === 0) {
+              pauseTimer();
+              return;
+            }
+            break;
+          case 0:
+            if (Number(minutes.textContent) > 0) {
+              seconds.textContent = "59";
+              minutes.textContent = `${Number(minutes.textContent) - 1}`.padStart(2, 0);
+            }
+            break;
+          default:
+            seconds.textContent = `${Number(seconds.textContent) - 1}`.padStart(2, 0);
+        } 
       }
     }
     countdown();
-  }, 1000);
-}
+  }, 250);
+};
 
 countdown();
